a7f9a0c2b7e387d331f157887917df3d5a79b45a
singalasreenivasulu55 <singalasreenivasulu55@gmail.com>
2024-02-12 19:19:28 +0530

    added 2nd script

--- roboshop.sh
+++ roboshop.sh
@@ -14,5 +14,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-  aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-0274360c37aefc4a6
+  aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-0274360c37aefc4a6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
